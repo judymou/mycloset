8613b8f8df38dba085850094863b129a698eb555
Judy Mou <moujudy@gmail.com>
2018-06-24 19:51:22 -0700

    add shop item

--- shop/models.py
+++ shop/models.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Item(models.Model):
+  image_url = models.CharField(max_length=512)
+  title = models.CharField(max_length=500)
+  description = models.CharField(max_length=1500)
+
+admin.site.register(Item)
