34c09e4b5af56702384873d51c33d74fb9e98cb6
Judy Mou <moujudy@gmail.com>
2018-06-24 21:25:20 -0700

    display item image urls

--- shop/views.py
+++ shop/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def index(request):
+  item_list = Item.objects.all()
+  return render(request, 'shop/index.html', {'item_list': item_list})
